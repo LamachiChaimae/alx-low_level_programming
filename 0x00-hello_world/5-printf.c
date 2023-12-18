ef3a49d65f32be343f3404ba1c85bcfc8e5178d3
ChaimaeLamachi <77735407+LamachiChaimae@users.noreply.github.com>
2023-12-18 15:50:55 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Description:  C program that prints
- * Return : always (0)
+ *
+ * Description: C program that prints a message
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-
-	printf("with proper grammar, but the outcome is a piece of art.\n");
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
