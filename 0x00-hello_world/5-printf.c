f507c8435fdb99e4c82f24cd2dcb51076483917b
LamachiChaimae <chaimae.lamachi17@gmail.com>
2023-12-18 06:51:45 -0800

    fff

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Description:  C program that prints
- * Return : always (0)
- */
+ * Description:  C program that prints with printf function.
+ * Return : always 0 (Success).
+*/
 
 int main(void)
 {
-
-	printf("with proper grammar, but the outcome is a piece of art.\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
