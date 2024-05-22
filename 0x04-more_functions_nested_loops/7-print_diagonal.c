84cad6348a7e9c91269212a7f467215e0e5dbe20
LamachiChaimae <chaimae.lamachi17@gmail.com>
2024-05-22 05:24:01 -0700

    task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print draws a diagonal line
+ * @n : umber of times the character
+*/
+
+void print_diagonal(int n)
+{
+	int post, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (post = 1; post <= n; post++)
+		{
+			for (space = 1; space <= post; space++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
