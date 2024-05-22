1a27e8dfcc7981d714952d8579a65e360333c934
ChaimaeLamachi <77735407+LamachiChaimae@users.noreply.github.com>
2024-05-22 13:33:22 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,20 +2,20 @@
 
 /**
  * print_diagonal - print draws a diagonal line
- * @n : umber of times the character
+ * @n : number of times the character
 */
 
 void print_diagonal(int n)
 {
-	int post, space;
+	int postn, space;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (post = 1; post <= n; post++)
+		for (postn = 1; postn <= n; postn++)
 		{
-			for (space = 1; space <= post; space++)
+			for (space = 1; space <= postn; space++)
 				_putchar(' ');
 			_putchar(92);
 			_putchar('\n');
