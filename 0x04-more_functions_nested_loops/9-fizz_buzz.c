5b21c051548400c4433f02f4e2b1396f4c0ea3b0
ChaimaeLamachi <77735407+LamachiChaimae@users.noreply.github.com>
2024-05-22 18:44:02 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - program that prints either number
- * or Fizz or Buzz or FizzBuzz
+ * main - program that prints either number,
+ * Fizz, Buzz, or FizzBuzz
  * Return: return 0
-*/
-
+ */
 int main(void)
 {
-	int num;
+    int num;
 
-	for (num = 0; num <= 100; numm++)
-	{
-		if ((num % 3 == 0) && !(num % 5 == 0))
-			printf("Fizz");
-		else if ((num % 5 == 0) && !(num % 3 == 0))
-			printf("Buzz");
-		else if (num % 5 == 0 && num % 3 == 0)
-			printf("FizzBuzz");
-		else
-			printf("%d", num);
-		if (num != 100)
-			printf(" ");
-		else
-			printf("\n");
-	}
-	return (0);
+    for (num = 1; num <= 100; num++)
+    {
+        if (num % 3 == 0 && num % 5 == 0)
+        {
+            printf("FizzBuzz");
+        }
+        else if (num % 3 == 0)
+        {
+            printf("Fizz");
+        }
+        else if (num % 5 == 0)
+        {
+            printf("Buzz");
+        }
+        else
+        {
+            printf("%d", num);
+        }
+        if (num != 100)
+        {
+            printf(" ");
+        }
+        else
+        {
+            printf("\n");
+        }
+    }
+    return 0;
 }
