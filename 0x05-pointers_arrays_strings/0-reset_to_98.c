cfd9e610aa3d221d48f16b2e668910d1d2bd7fc6
ChaimaeLamachi <77735407+LamachiChaimae@users.noreply.github.com>
2024-05-24 21:47:26 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value
- * it points to to 98
+ * reset_to_98 - updates the value of the varaible
  * @n : pointer to the variable to update
 */
 
