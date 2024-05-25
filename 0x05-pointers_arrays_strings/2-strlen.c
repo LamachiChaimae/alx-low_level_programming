1bfe2b7b8b9374b27a7bdd7c9c80484100b6d970
LamachiChaimae <chaimae.lamachi17@gmail.com>
2024-05-25 04:13:50 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s : string
+ * Return: the length of the string
+*/
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; i++)
+		++counter;
+	return (counter);
+}
