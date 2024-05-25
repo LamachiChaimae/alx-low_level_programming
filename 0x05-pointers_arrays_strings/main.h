d1fa5eccdb4d193f3d474aa501b66192a3e92793
ChaimaeLamachi <77735407+LamachiChaimae@users.noreply.github.com>
2024-05-25 11:57:15 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 #include <stdio.h>
 
@@ -14,11 +14,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s)void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /* MAIN_H */
+#endif
